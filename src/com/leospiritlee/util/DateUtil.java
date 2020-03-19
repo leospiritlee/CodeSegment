239b927bc5c020bda01950837173f7cf81765c9e
leospiritlee <leospiritlee@gmail.com>
2020-03-19 22:28:20 +0800

    init code

---
+++ src/com/leospiritlee/util/DateUtil.java
@@ -0,0 +1,14 @@
+package com.leospiritlee.util;
+
+/**
+ * @Project: CodeSegment
+ * @ClassName DateUtil
+ * @description: 时间工具类
+ * @author: leospiritlee
+ * @create: 2020-03-19 22:23
+ **/
+public class DateUtil {
+
+
+
+}
