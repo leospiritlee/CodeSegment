0cedb2ab581f080822b46e447dad13d7c4232e7f
leospiritlee <leospiritlee@gmail.com>
2020-06-26 21:26:27 +0800

    init code

--- src/main/java/com/leospiritlee/leetCode/arrays/Solution.java
+++ src/main/java/com/leospiritlee/leetCode/arrays/Solution.java
@@ -1,19 +1,24 @@
 package com.leospiritlee.leetCode.arrays;
 
+import com.sun.org.apache.bcel.internal.generic.ARETURN;
 import sun.reflect.generics.tree.Tree;
 
 import java.lang.reflect.Array;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
+import java.util.TreeMap;
 
 /**
  * @Project: CodeSegment
@@ -32,13 +37,482 @@ public class Solution {
 
 //        System.out.println(solution.findBestValue(n, target));
 
-        int[] nums1 = new int[]{1,2,3,0,0,0};
-        int[] nums2 = new int[]{2,5,6};
-        int[] index = new int[]{0};
+        int[] nums1 = new int[]{2,21,43,38,0,42,33,7,24,13,12,27,12,24,5,23,29,48,30,31};
+        int[] nums2 = new int[]{2,42,38,0,43,21};
+        int[] index = new int[]{2, 7, 11, 15};
+        int target1 = 9;
 
+        String[] arrayS = new String[]{"cat","bt","hat","tree"};
+        String chars = "atach";
 //        System.out.println(Arrays.toString(solution.createTargetArray(nums,index)));
-        System.out.println(generate(3));
+//        System.out.println(relativeSortArray(nums1, nums2));
 //        merge(nums1,3,nums2,3);
+//        System.out.println(Arrays.toString(sumZero(4)));
+        System.out.println(Arrays.toString(twoSum(index, target1)));
+    }
+
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int right=0;right<nums.length;right++){
+            if(map.put(nums[right],right)!=null){
+                return true;
+            }
+            if(map.size()>k){
+                map.remove(nums[right-k]);
+            }
+        }
+        return false;
+
+    }
+
+
+    public static int[] twoSum(int[] numbers, int target) {
+        if (numbers == null){
+            return null;
+        }
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                return new int[]{i + 1, j + 1};
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return null;
+    }
+
+
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            if(map.containsKey(num)){
+                map.put(num, map.get(num) + 1);
+                if(map.get(num) > 1){
+                    return true;
+                }
+
+            }else {
+                map.put(num, 1);
+            }
+        }
+
+
+        return false;
+    }
+
+    public static int findShortestSubArray(int[] nums) {
+
+        Map<Integer, Integer> left = new HashMap(),
+                right = new HashMap(), count = new HashMap();
+
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            if (left.get(x) == null) left.put(x, i);
+            right.put(x, i);
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+
+        int ans = nums.length;
+        int degree = Collections.max(count.values());
+        for (int x: count.keySet()) {
+            if (count.get(x) == degree) {
+                ans = Math.min(ans, right.get(x) - left.get(x) + 1);
+            }
+        }
+        return ans;
+    }
+
+
+
+    public static int maxSubArray(int[] nums) {
+        int[] arr = new int[nums.length];
+        arr[0] = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1, len = nums.length; i < len; i++) {
+            int max = arr[i-1];
+            int sum = 0;
+            for (int j = i; j >= 0; j--) {
+                sum += nums[j];
+                max = Math.max(sum, max);
+            }
+            arr[i] = max;
+            maxSum = Math.max(maxSum, max);
+        }
+        return maxSum;
+    }
+
+
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int tmpCompare = 0;
+        for(int num : nums){
+            if(num == 1){
+                count ++;
+            }else{
+                if(tmpCompare == 0){
+                    tmpCompare = count;
+                }else{
+                    if(count > tmpCompare){
+                        tmpCompare = count;
+                        count = 0;
+                    }
+                }
+                count = 0;
+            }
+        }
+
+        return count > tmpCompare ? count : tmpCompare;
+    }
+
+
+    public int search(int[] nums, int target) {
+        int count = 0;
+        for(int num : nums){
+            if(num == target){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int missingNumber(int[] nums) {
+        int a = 1;
+        int b = nums.length;
+        int sum = (a+b) * (b-a+1) / 2;
+
+        int numsSum = 0;
+        for(int num : nums){
+            numsSum += num;
+        }
+
+        return sum - numsSum;
+    }
+
+
+
+    public static int fib(int N) {
+        if(N == 0 || N ==1){
+            return N;
+        }
+
+        int[] numArray = new int[N+1];
+        numArray[0] = 0;
+        numArray[1] = 1;
+        for(int i = 2; i < N + 1; i++){
+            numArray[i] = numArray[i-1] + numArray[i-2];
+        }
+
+
+
+        return numArray[N];
+    }
+
+
+    public static int countCharacters(String[] words, String chars) {
+
+        int sum = 0;
+        for(String word : words){
+            boolean flag = true;
+            String[] wordArray = word.split("");
+            for(int i = 0; i < wordArray.length; i++){
+                if(chars.indexOf(wordArray[i]) < 0){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                sum += word.length();
+            }
+
+        }
+
+        return sum;
+    }
+
+    public static int[] sumZero(int n) {
+
+        int[] result = new int[n];
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            if(i == n -1){
+                result[i] = 0-sum;
+                break;
+            }
+            result[i] = i ;
+            sum +=i;
+        }
+
+
+        return result;
+    }
+
+    public static String dayOfTheWeek(int day, int month, int year) {
+        long y = year;
+        long m = month;
+        long total = 0;
+        total += 365 * y;
+        if (y >= 0) {
+            total += (y + 3) / 4 - (y + 99) / 100 + (y + 399) / 400;
+        } else {
+            total -= y / -4 - y / -100 + y / -400;
+        }
+        total += ((367 * m - 362) / 12);
+        total += day - 1;
+        if (m > 2) {
+            total--;
+            if (isLeapYear(y) == false) {
+                total--;
+            }
+        }
+        total =  total - ((146097 * 5L) - (30L * 365L + 7L));
+
+
+        int dow0 = (int)Math.floorMod(total + 3, 7) + 1;
+
+        switch (dow0){
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            case 7:
+                return "Sunday";
+            default:
+                return "";
+        }
+
+    }
+
+
+    public static boolean isLeapYear(long prolepticYear) {
+        return ((prolepticYear & 3) == 0) && ((prolepticYear % 100) != 0 || (prolepticYear % 400) == 0);
+    }
+
+    public int findSpecialInteger(int[] arr) {
+
+        int threshold = arr.length / 4;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i + threshold] == arr[i]) {
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+
+
+    public void moveZeroes(int[] nums) {
+       if(nums == null){
+           return;
+       }
+
+       int j = 0;
+       for(int num : nums){
+           if(num != 0){
+               nums[j++] = num;
+           }
+       }
+
+       while (j < nums.length){
+           nums[j++] = 0;
+       }
+
+    }
+
+
+
+    public static int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] newArray = new int[arr1.length];
+        TreeMap<Integer, Integer> countMap = new TreeMap<>();
+        for(int num : arr1){
+            if(countMap.containsKey(num)){
+                countMap.put(num, countMap.get(num) + 1);
+            }else {
+                countMap.put(num, 1);
+            }
+        }
+
+        System.out.println(countMap);
+
+        int tmpIndex = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            int tmp = arr2[i];
+            int count = countMap.get(tmp) == null ? 1 : countMap.remove(tmp);
+            for (int j = 0; j < count; j++) {
+                newArray[tmpIndex] = tmp;
+                tmpIndex++;
+            }
+        }
+
+        if(countMap.size() > 0){
+            for(int k : countMap.keySet()){
+                int v  = countMap.get(k);
+               for(int m = 0; m <v; m++){
+                   newArray[tmpIndex] = k;
+                   tmpIndex++;
+               }
+            }
+        }
+
+
+        return newArray;
+    }
+
+
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        int len = nums.length /2;
+
+        for(int num : nums){
+            if(countMap.containsKey(num)){
+                countMap.put(num, countMap.get(num) + 1);
+            }else {
+                countMap.put(num, 1);
+            }
+        }
+
+        for(Integer k : countMap.keySet()){
+            if(countMap.get(k) > len){
+                return k;
+            }
+        }
+
+
+        return -1;
+    }
+
+    public void merge_1(int[] A, int m, int[] B, int n) {
+        System.arraycopy(B,0, A, m, n);
+        Arrays.sort(A);
+    }
+
+
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(matrix[i][j] == target){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    public int[] sortedSquares(int[] A) {
+
+        for(int i = 0, size = A.length; i < size; i++){
+            A[i] = A[i] * A[i];
+        }
+
+        Arrays.sort(A);
+
+        return A;
+    }
+
+
+    public ListNode removeDuplicateNodes(ListNode head) {
+
+        if (head == null) {
+            return head;
+        }
+        Set<Integer> occurred = new HashSet<Integer>();
+        occurred.add(head.val);
+        ListNode pos = head;
+        // 枚举前驱节点
+        while (pos.next != null) {
+            // 当前待删除节点
+            ListNode cur = pos.next;
+            if (occurred.add(cur.val)) {
+                pos = pos.next;
+            } else {
+                pos.next = pos.next.next;
+            }
+        }
+        pos.next = null;
+        return head;
+    }
+
+
+    public int[] sortArrayByParityII(int[] A) {
+
+        int length = A.length;
+        int[] newArray = new int[length];
+
+        int k = 0;
+        int m = 1;
+        for(int a : A){
+
+            if(a % 2 ==0){
+                newArray[k] = a;
+                k+=2;
+            }else if(a % 2 == 1){
+                newArray[m] = a;
+                m+=2;
+            }
+        }
+
+
+        return newArray;
+    }
+
+
+    public int xorOperation(int n, int start) {
+        int sum = 0;
+        while (start < n){
+            start++;
+            sum  =(start + 2 * start);
+        }
+        return sum;
+    }
+
+    public int[] runningSum(int[] nums) {
+
+        for(int i = 1, size = nums.length; i < size; i++){
+            nums[i] += nums[i-1];
+        }
+
+        return nums;
+    }
+
+
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        return Arrays.equals(target, arr);
+    }
+
+
+    public String addBinary(String a, String b) {
+        StringBuffer ans = new StringBuffer();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
+        }
+
+        if (carry > 0) {
+            ans.append('1');
+        }
+        ans.reverse();
+
+        return ans.toString();
     }
 
     public static List<List<Integer>> generate(int numRows) {
